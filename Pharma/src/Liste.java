c549d40c28be1396404cce460d2f3cecd7333d03
Timéo <timeo.gales@gmail.com>
2025-02-07 09:52:07 +0100

    add docstring

--- Pharma/src/Liste.java
+++ Pharma/src/Liste.java
@@ -1,6 +1,16 @@
 import java.util.List;
 
 public class Liste {
+    /**
+     * Recherche un produit dans une liste et affiche un message indiquant s'il a été trouvé ou non.
+     *
+     * Cette méthode parcourt la liste des produits et vérifie si le produit recherché est présent.
+     * Si le produit est trouvé, un message de confirmation est affiché. Sinon, un message indiquant
+     * son absence est retourné.
+     *
+     * @param produits         La liste contenant les noms des produits disponibles.
+     * @param produitRecherche Le nom du produit à rechercher dans la liste.
+     */
     public static void rechercherProduit(List<String> produits, String produitRecherche) {
         boolean trouve = false;
         for (String produit : produits) {
