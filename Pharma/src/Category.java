7f1261bb61a2bcc8429c78f6bf0eac1b2c206919
Timéo <timeo.gales@gmail.com>
2025-02-07 11:05:40 +0100

    Update Category.java

--- Pharma/src/Category.java
+++ Pharma/src/Category.java
@@ -1,19 +1,33 @@
+/**
+ * Représente une catégorie avec un nom.
+ */
 public class Category {
     private String name;
 
+    /**
+     * Constructeur pour initialiser la catégorie avec un nom.
+     * 
+     * @param name Le nom de la catégorie.
+     */
     public Category(String name) {
         this.name = name;
     }
 
+    /**
+     * Récupère le nom de la catégorie.
+     * 
+     * @return Le nom de la catégorie.
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Définit le nom de la catégorie.
+     * 
+     * @param name Le nom de la catégorie à définir.
+     */
     public void setName(String name) {
         this.name = name;
     }
 }
-
-
-
-
